af30a226b18254e4cdf41ffdb7ce3fd36bf62bf6
Mansur Mahammad - LFI <mmahammad@linkfields.com>
2023-08-03 18:20:22 +0530

    Added token

--- cms/config.tsx
+++ cms/config.tsx
@@ -5,6 +5,8 @@ const config = {
   backend: {
     name: "git-gateway",
     branch: "master",
+    app_id: "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",
+    access_token: "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"
   },
   media_folder: "public/images",
   public_folder: "images",
